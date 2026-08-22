c66d5ffef75fe964a019a20d0e0051a18e6a5549
Reza <rezacalm993@gmail.com>
2026-08-22 08:59:53 +0330

    deploy: update TeamPulse

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,5 +7,6 @@ const dbPath = path.resolve(__dirname, '..', process.env.DB_PATH || './database/
 const db = new Database(dbPath);
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
+db.pragma('busy_timeout = 5000');
 
 module.exports = db;
