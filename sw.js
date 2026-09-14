1152c82f5291c8bc594e73f71db25433cc934b99
Reza <rezacalm993@gmail.com>
2026-09-14 09:11:04 +0330

    deploy: update TeamPulse

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'team-pulse-static-v240';
+const CACHE = 'team-pulse-static-v241';
 // Keep install tiny. Versioned JS/CSS are cached on first fetch via cacheFirst.
 const CORE_ASSETS = [
   '/app',
