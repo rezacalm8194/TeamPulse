c1bf356230c47bab3119c4549bbe32bca2c1d04a
Reza <rezacalm993@gmail.com>
2026-09-19 15:35:03 +0330

    deploy: update TeamPulse

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const TP_ASSET_V = 'tp265';
+const TP_ASSET_V = 'tp266';
 window._tpChunkReady = Object.create(null);
 window._tpChunkPromise = Object.create(null);
 function _tpChunkSrc(file) { return '/' + file + '?v=' + TP_ASSET_V; }
